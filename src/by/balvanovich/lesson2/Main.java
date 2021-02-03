963739663e444f60c1e59aa34c2e48e47003cffb
TBalvanovich <t.balvanovich@gmail.com>
2021-02-03 14:09:48 +0300

    add Task4

---
+++ src/by/balvanovich/lesson2/Main.java
@@ -0,0 +1,24 @@
+package by.balvanovich.lesson2;
+
+import com.sun.javaws.IconUtil;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+            int a = 123456;
+            int b = a;
+            int count = 0;
+
+            while (b > 0) {
+                count++;
+                b = b / 10;
+            }
+            int mass[] = new int[count];
+            for (int i = 0; i < count; i++) {
+                mass[i] = a % 10;
+                a = a / 10;
+                System.out.print(mass[i]);
+            }
+}
+    }
